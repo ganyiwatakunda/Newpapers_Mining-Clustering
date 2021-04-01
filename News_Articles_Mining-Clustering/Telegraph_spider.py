467f453e88c2e77ec537a228d99caa672c95dfaf
Kelvin Chumbe <43356490+kelvinchumbe@users.noreply.github.com>
2021-04-01 12:21:41 +0200

    Second Commit

--- News_Articles_Mining-Clustering/Telegraph_spider.py
+++ News_Articles_Mining-Clustering/Telegraph_spider.py
@@ -15,7 +15,6 @@ class Telegraph(scrapy.Spider):
         for category in news_categories:
             if category.split("/")[1] in categories_to_scrap:
                 category_url = response.url + category
-                print(category_url)
 
                 yield scrapy.Request(category_url, callback=self.parse_articles, cb_kwargs=dict(category=category.split("/")[1]))
 
@@ -24,51 +23,14 @@ class Telegraph(scrapy.Spider):
         
         for url in article_urls:
             article_url = "https://www.telegraph.co.uk" + url
-            print(article_url)
 
-            yield scrapy.Request(article_url, callback=self.parse_story, cb_kwargs=dict(category=category, url=article_url))
+            yield scrapy.Request(article_url, callback=self.parse_story, cb_kwargs=dict(category=category))
 
 
-    def parse_story(self, response, category, url):
+    def parse_story(self, response, category):
         heading = response.css('article > header > h1::text').get()
-        subheading = response.css('article > header > p::text').get()
-        author = response.css('article > div.grid-col.grid-col-12.article__layout.article__layout--header-meta.article__separator > div > span > span > a > span.e-byline__author::text').get()
-        datetime = response.css('article > div.grid-col.grid-col-12.article__layout.article__layout--header-meta.article__separator > div > time::text').get()
         article = response.css('article > div.grid-col.grid-col-12.article__layout.article__layout--content > div:nth-child(1) > div.articleBodyText.section > div.component.article-body-text > p::text').getall()
 
         article = ' '.join([paragraph for paragraph in article])
 
-        yield dict(url=url, category=category, heading=heading, subheading=subheading, author=author, datetime=datetime, article=article)
-
-
-
-
-
-
-    
-# Get links to different sections
-# response.css('div.site-header__primary-navigation-wrapper > div > nav > ul > li > a::attr(href)').getall()
-# Filter for business, politics, culture, sports
-# Sample link: https://www.telegraph.co.uk/politics/
-
-
-# Get links to each story on the category on that page
-# response.css('section > ul > li > article > div.card__content > h3 > a::attr(href)').getall()
-
-
-# Get heading of story
-# response.css('article > header > h1::text').get()
-
-
-# Get subheading
-# response.css('article > header > p::text').get()
-
-
-# Get author
-# response.css('article > div.grid-col.grid-col-12.article__layout.article__layout--header-meta.article__separator > div > span > span > a > span.e-byline__author::text').get()
-
-# Get Date and Time
-# response.css('article > div.grid-col.grid-col-12.article__layout.article__layout--header-meta.article__separator > div > time::text').get()
-
-# Get the article
-# response.css('article > div.grid-col.grid-col-12.article__layout.article__layout--content > div:nth-child(1) > div.articleBodyText.section > div.component.article-body-text > p::text').getall()
+        yield dict(url=response.url, category=category, heading=heading, article=article)
--- News_Articles_Mining-Clustering/web_app.py
+++ News_Articles_Mining-Clustering/web_app.py
@@ -4,7 +4,6 @@ import pickle
 
 from preprocessing_utils import *
 
-
 st.write(""" 
     # Cluster News Articles
 """)
@@ -16,9 +15,10 @@ news_article = st.text_input("Paste a news article: ")
 news_category = st.text_input("Enter the category of the news article: Allowed categories are [politics, business, culture, sports]")
 news_url = st.text_input("Enter link to news article: ")
 
-if news_article and news_category:
+if news_article and news_category and news_url:
     st.write("**News Article **: " , news_article)
     st.write("**Category **:" , news_category)
+    st.write("**Link to Article **:" , news_url)
 
     k_means = pickle.load(open('kmeans_model.pkl', 'rb'))
     tfidf_vectorizer = pickle.load(open('tfidf_vectorizer.pkl', 'rb'))
@@ -32,37 +32,37 @@ if news_article and news_category:
     st.write('NEWS CLUSTERS')
 
     #Articles in cluster 0
-    cluster_0 = articles[articles['clusters'] == 0][['category', 'article', 'clusters']]
+    cluster_0 = articles[articles['clusters'] == 0][['category', 'article', 'clusters', 'url']]
     st.write('Cluster 0')
     st.write(cluster_0)
 
     #Articles in cluster 1
-    cluster_1 = articles[articles['clusters'] == 1][['category', 'article', 'clusters']]
+    cluster_1 = articles[articles['clusters'] == 1][['category', 'article', 'clusters', 'url']]
     st.write('Cluster 1')
     st.write(cluster_1)
 
     #Articles in cluster 2
-    cluster_2 = articles[articles['clusters'] == 2][['category', 'article', 'clusters']]
+    cluster_2 = articles[articles['clusters'] == 2][['category', 'article', 'clusters', 'url']]
     st.write('Cluster 2')
     st.write(cluster_2)
 
     #Articles in cluster 3
-    cluster_3 = articles[articles['clusters'] == 3][['category', 'article', 'clusters']]
+    cluster_3 = articles[articles['clusters'] == 3][['category', 'article', 'clusters', 'url']]
     st.write('Cluster 3')
     st.write(cluster_3)
 
     #Articles in cluster 4
-    cluster_4 = articles[articles['clusters'] == 4][['category', 'article', 'clusters']]
+    cluster_4 = articles[articles['clusters'] == 4][['category', 'article', 'clusters', 'url']]
     st.write('Cluster 4')
     st.write(cluster_4)
 
     #Articles in cluster 5
-    cluster_5 = articles[articles['clusters'] == 5][['category', 'article', 'clusters']]
+    cluster_5 = articles[articles['clusters'] == 5][['category', 'article', 'clusters', 'url']]
     st.write('Cluster 5')
     st.write(cluster_5)
 
     #Articles in cluster 6
-    cluster_6 = articles[articles['clusters'] == 6][['category', 'article', 'clusters']]
+    cluster_6 = articles[articles['clusters'] == 6][['category', 'article', 'clusters', 'url']]
     st.write('Cluster 6')
     st.write(cluster_6)
 
@@ -70,36 +70,29 @@ if st.button("Cluster Article"):
     cluster = k_means.predict(tfidf_pca_comp)
 
     if cluster == 0:
-        cluster_0.insert(0, {'category': news_category, 'article': news_article, 'clusters': cluster})
         st.write('Your article belongs to cluster: ', str(cluster))
         st.write(cluster_0)
 
     elif cluster == 1:
-        cluster_1.insert(0, {'category': news_category, 'article': news_article, 'clusters': cluster})
         st.write('Your article belongs to cluster: ', str(cluster))
         st.write(cluster_1)
 
     elif cluster == 2:
-        cluster_2.insert(0, {'category': news_category, 'article': news_article, 'clusters': cluster})
         st.write('Your article belongs to cluster: ', str(cluster))
         st.write(cluster_2)
 
     elif cluster == 3:
-        cluster_3.insert(0, {'category': news_category, 'article': news_article, 'clusters': cluster})
         st.write('Your article belongs to cluster: ', str(cluster))
         st.write(cluster_3)
 
     elif cluster == 4:
-        cluster_4.insert(0, {'category': news_category, 'article': news_article, 'clusters': cluster})
         st.write('Your article belongs to cluster: ', str(cluster))
         st.write(cluster_4)
 
     elif cluster == 5:
-        cluster_5.insert(0, {'category': news_category, 'article': news_article, 'clusters': cluster})
         st.write('Your article belongs to cluster: ', str(cluster))
         st.write(cluster_5)
     
     elif cluster == 6:
-        cluster_6.insert(0, {'category': news_category, 'article': news_article, 'clusters': cluster})
         st.write('Your article belongs to cluster: ', str(cluster))
         st.write(cluster_6)
